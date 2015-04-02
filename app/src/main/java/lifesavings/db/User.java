681aa83c479ee39f3965d48616f985743697ba34
Joshuaaspencer <Joshuaaspencer@gmail.com>
2015-04-02 17:52:17 -0400

    user updated - josh

--- app/src/main/java/lifesavings/db/User.java
+++ app/src/main/java/lifesavings/db/User.java
@@ -10,7 +10,7 @@ public class User {
     private String gender;
     private int weight;
     private int height;
-    private int bmi;
+    private double bmi;
     private int category;
 
     public User() {
@@ -27,7 +27,89 @@ public class User {
         this.category = category;
         this.bfp = bfp;
     }
+    public User(int userid, String name, String gender, int weight, int height, int bmi, int category) {
+        this.userid = userid;
+        this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.bmi = bmi;
+        this.category = category;
+        this.bfp = -1;
+    }
+    public User(int userid, String name, String gender, int weight, int height, int bmi) {
+        this.userid = userid;
+        this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.bmi = bmi;
+        this.category = calcCategory(this.bmi);
+        this.bfp = -1;
+    }
 
+    public User (int userid, String name, String gender, int weight, int height){
+        this.userid = userid;
+        this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.bmi = calcBMI(weight,height);
+        this.category = calcCategory(this.bmi);
+        this.bfp = -1;
+    }
+    public static double calcBMI(int weight, int height){
+        // dw = double weight, dh = double height for accuracy reasons.
+        // bmi = (mass(lbs)/height(inches)) * 703
+        double dw = (double)weight;
+        double dh = (double)height;
+        return (dw/dh)*703.0;
+    }
+    public static int calcCategory(double bmi){
+        int rank;
+        rank  = -1;
+        if(bmi < 16.0)
+        {
+            //severe thinness
+            rank = 1;
+        }
+        else if (bmi >= 16.0 && bmi < 17.0)
+        {
+            //moderate thinness
+            rank = 2;
+        }
+        else if (bmi >= 17.0 && bmi < 18.5)
+        {
+            //mild thinness
+            rank = 3;
+        }
+        else if (bmi >= 18.5 && bmi < 25)
+        {
+            //normal
+            rank = 4;
+        }
+        else if (bmi >= 25 && bmi < 30)
+        {
+            //overweight
+            rank = 5;
+        }
+        else if (bmi >= 30 && bmi < 35)
+        {
+            //obese class I
+            rank = 6;
+        }
+        else if (bmi >= 35 && bmi < 40)
+        {
+            // obese class II
+            rank = 7;
+        }
+        else
+        {
+            // obese class II
+            rank = 8;
+        }
+        return rank;
+    }
     public double getBfp() {
         return bfp;
     }
@@ -44,7 +126,7 @@ public class User {
         this.category = category;
     }
 
-    public int getBmi() {
+    public double getBmi() {
         return bmi;
     }
 
